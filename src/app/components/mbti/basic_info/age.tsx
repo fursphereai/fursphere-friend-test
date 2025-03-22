27ea4267c3c60cbfebb785af0cd90009cd900121
keyi li <keyili@keyis-MacBook-Pro.local>
2025-03-22 16:23:45 -0500

    jason's update

--- src/app/components/mbti/basic_info/age.tsx
+++ src/app/components/mbti/basic_info/age.tsx
@@ -178,7 +178,7 @@ const Age: React.FC<AgeProps> = ({ handleNext, handleBack, step, setStep, survey
                       </div>
                       <div className="relative mx-[20px] h-[200px] overflow-hidden">
                         <div className="absolute inset-0  flex items-center justify-center pointer-events-none">
-                          <div className=" w-full h-[50px] bg-[#D1D7EF] opacity-20" />
+                          <div className=" w-full h-[50px] bg-[#D1D7EF] opacity-20 z-50" />
                         </div>
                         <div 
                           className="h-full bg-[#FFFFFF] rounded-[6px] overflow-auto snap-y snap-mandatory"
